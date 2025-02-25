6e27f0bcf38f5f453fdce58015f4c5f1e58512bc
Ej19748 <ej19748@email.vccs.edu>
2025-02-25 02:07:45 +0000

    Code

--- .ccls-cache/@home@runner@CPlusPlus/main.cpp
+++ .ccls-cache/@home@runner@CPlusPlus/main.cpp
@@ -1,30 +1,72 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
 using namespace std;
 
-int *reverseArray(int[], const int);
-void displayArray(int *, const int);
-
-int main() {
-  const int ARRAY_SIZE = 10;
-  int numbers[ARRAY_SIZE] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-  int *numbers_reversed = reverseArray(numbers, ARRAY_SIZE);
-  cout << endl;
-  displayArray(numbers, ARRAY_SIZE);
-  cout << endl;
-  displayArray(numbers_reversed, ARRAY_SIZE);
-  delete[] numbers_reversed;
-  numbers_reversed = nullptr;
-
+struct Student {
+string name;
+int idNum;
+int* tests;
+double average;
+char grade;
+};
+void calculateGrades(Student* students, int numStudents, int numTests);
+void displayGrades(const Student* students, int numStudents);
+int main()
+{
+  ifstream inputFile("test_data.txt");
+  if (!inputFile) {
+    cout << "Error opening file." << endl;
+    return 1;
+  }
+  int numStudents, numTests;
+  inputFile >> numStudents >> numTests;
+  Student* students = new Student[numStudents];
+  for (int i = 0; i < numStudents; i++)
+    {
+      inputFile >> students[i].name >> students[i].idNum;
+      students[i].tests = new int[numTests];
+      for (int j = 0; j < numTests; j++)
+        {
+          inputFile >> students[i].tests[j];
+        }
+    }
+  inputFile.close();
+  calculateGrades(students, numStudents, numTests);
+  displayGrades(students, numStudents);
+  for (int i = 0; i < numStudents; i++)
+    {
+      delete[] students[i].tests;
+    }
+  delete[] students;
   return 0;
 }
-int *reverseArray(int array[], const int SIZE) {
-  int *newArray = new int[SIZE];
-  int j = (SIZE - 1);
-  for (int i = 0; i < SIZE; i++, j--)
-    *(newArray + i) = array[j];
-  return newArray;
+void calculateGrades(Student* students, int numStudents, int numTests)
+{
+  for (int i = 0; i < numStudents; i++)
+    {
+      int total = 0;
+      for (int j = 0; j < numTests; j++)
+        {
+          total += students[i].tests[j];
+        }
+      students[i].average = static_cast<double>(total) / numTests;
+      if (students[i].average >= 91)
+        students[i].grade = 'A';
+      else if (students[i].average >= 81)
+        students[i].grade = 'B';
+      else if (students[i].average >= 71)
+        students[i].grade = 'C';
+      else if (students[i].average >= 61)
+        students[i].grade = 'D';
+      else
+        students[i].grade = 'F';
+    }
 }
-void displayArray(int *array, const int SIZE) {
-  for (int i = 0; i < SIZE; i++)
-    cout << *(array + i) << " ";
+void displayGrades(const Student* students, int numStudents)
+{
+  cout << left << setw(12) << "Name" << setw(8) << "ID" << setw(10) << "Average" << setw(8) << "Grade" << endl;
+  for (int i = 0; i < numStudents; i++)
+    cout << left << setw(12) << students[i].name << setw(8) << students[i].idNum << setw(10) << fixed << setprecision(2) << students[i].average << students[i].grade << endl;
 }
