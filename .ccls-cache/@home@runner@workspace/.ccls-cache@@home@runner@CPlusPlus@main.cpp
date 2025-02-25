b9c46322d112522132bff0ff7f7110b9dc0d4da3
Ej19748 <ej19748@email.vccs.edu>
2025-02-25 02:10:45 +0000

    Comments

--- .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
+++ .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <fstream>
 using namespace std;
-
+// Structure to store student data
 struct Student {
 string name;
 int idNum;
@@ -11,6 +11,7 @@ int* tests;
 double average;
 char grade;
 };
+// Function to read student data from file
 void calculateGrades(Student* students, int numStudents, int numTests);
 void displayGrades(const Student* students, int numStudents);
 int main()
@@ -22,10 +23,13 @@ int main()
   }
   int numStudents, numTests;
   inputFile >> numStudents >> numTests;
+  // Dynamically allocate memory for student data
   Student* students = new Student[numStudents];
+  // Read student data from file
   for (int i = 0; i < numStudents; i++)
     {
       inputFile >> students[i].name >> students[i].idNum;
+      // Dynamically allocate memory for test scores
       students[i].tests = new int[numTests];
       for (int j = 0; j < numTests; j++)
         {
@@ -33,8 +37,11 @@ int main()
         }
     }
   inputFile.close();
+  // Calculate average test score and grade for each student
   calculateGrades(students, numStudents, numTests);
+  // Display student data and grades
   displayGrades(students, numStudents);
+  // Free dynamically allocated memory
   for (int i = 0; i < numStudents; i++)
     {
       delete[] students[i].tests;
@@ -42,6 +49,7 @@ int main()
   delete[] students;
   return 0;
 }
+// Function to calculate average test score and grade for each student
 void calculateGrades(Student* students, int numStudents, int numTests)
 {
   for (int i = 0; i < numStudents; i++)
@@ -51,6 +59,7 @@ void calculateGrades(Student* students, int numStudents, int numTests)
         {
           total += students[i].tests[j];
         }
+      // Calculate average test score
       students[i].average = static_cast<double>(total) / numTests;
       if (students[i].average >= 91)
         students[i].grade = 'A';
@@ -64,6 +73,7 @@ void calculateGrades(Student* students, int numStudents, int numTests)
         students[i].grade = 'F';
     }
 }
+// Function to display student data and grades
 void displayGrades(const Student* students, int numStudents)
 {
   cout << left << setw(12) << "Name" << setw(8) << "ID" << setw(10) << "Average" << setw(8) << "Grade" << endl;
